169a7f27571f6ead8ab11d131c7e4f959c1f0cda
Zak Kastl <zkastl8@gmail.com>
2023-08-04 16:52:54 -0500

    more updates to perlin

--- image_magic.py
+++ image_magic.py
@@ -8,18 +8,20 @@ scale = 100.
 octaves = 6
 persistence = 0.5
 lacunarity = 2.0
+RANGE_MAX = 10
 
-world = np.zeros(shape)
-for i in range(shape[0]):
-    for j in range(shape[1]):
-        world[i][j] = noise.pnoise2(i/scale,
+for map in range(RANGE_MAX):
+    world = np.zeros(shape)
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            world[i][j] = noise.pnoise2(i/scale,
                                     j/scale,
-                                    octaves=octaves,
+                                    octaves=map + 1,
                                     persistence=persistence,
                                     lacunarity=lacunarity,
                                     repeatx=1024,
                                     repeaty=1024,
                                     base=0)
 
-formatted_world = (world * 255 / np.max(world)).astype('uint8')
-image = Image.fromarray(formatted_world).save('./noise.png')
+    formatted_world = (world * 255 / np.max(world)).astype('uint8')
+    image = Image.fromarray(formatted_world).save('./noise_' + str(map) + '.png')
