e640ff7df263e48fd16ac2db4fc6e6bfd1cb3992
Zak Kastl <zkastl8@gmail.com>
2023-08-17 16:04:05 -0500

    added more ideas, probably a bug somewhere

--- subtle_color_removal.py
+++ subtle_color_removal.py
@@ -13,6 +13,7 @@ counter = 0
 for files in os.scandir(image_root):
     file_name = os.path.splitext(files.name)[0]
     image = cv2.imread(image_root + file_name + '.jpg')
+    negative = cv2.bitwise_not(image)
 
     # Convert the image to the Lab color space
     lab_image = cv2.cvtColor(image, cv2.COLOR_BGR2Lab) # Calculate color gradients
@@ -23,17 +24,35 @@ for files in os.scandir(image_root):
     subtle_pattern_mask = gradient_magnitude > threshold # Apply the mask to the original image
     subtle_pattern = image.copy()
     subtle_pattern[~subtle_pattern_mask] = [0, 0, 0]  # Set non-pattern areas to black
+
+    # Convert the negative to the Lab color space
+    lab_image_neg = cv2.cvtColor(negative, cv2.COLOR_BGR2Lab) # Calculate color gradients
+    gradient_x = cv2.Sobel(lab_image_neg[:, :, 1], cv2.CV_64F, 1, 0, ksize=3)
+    gradient_y = cv2.Sobel(lab_image_neg[:, :, 1], cv2.CV_64F, 0, 1, ksize=3) # Calculate gradient magnitude
+    gradient_magnitude = np.sqrt(gradient_x**2 + gradient_y**2) # Threshold the gradient magnitude to find subtle patterns
+    threshold = 30  # Adjust this threshold based on your requirements
+    subtle_pattern_mask = gradient_magnitude > threshold # Apply the mask to the original image
+    subtle_pattern_neg = image.copy()
+    subtle_pattern_neg[~subtle_pattern_mask] = [0, 0, 0]  # Set non-pattern areas to black
     
     # Save or display the result
-    fig, axs = plt.subplots(1, 2, figsize=(10,5))
+    fig, axs = plt.subplots(2, 2, figsize=(10,5))
     
-    axs[0].imshow(image)
-    axs[0].set_title('Original Image')
-    axs[0].axis('off')
+    axs[0][0].imshow(image)
+    axs[0][0].set_title('Original Image')
+    axs[0][0].axis('off')
 
-    axs[1].imshow(subtle_pattern)
-    axs[1].set_title('Subtle Pattern Image')
-    axs[1].axis('off')
+    axs[0][1].imshow(negative)
+    axs[0][1].set_title('Image Negative')
+    axs[0][1].axis('off')
+
+    axs[1][0].imshow(subtle_pattern)
+    axs[1][0].set_title('Subtle Pattern Image')
+    axs[1][0].axis('off')
+
+    axs[1][1].imshow(subtle_pattern_neg)
+    axs[1][1].set_title('Subtle Pattern Image (Negative)')
+    axs[1][1].axis('off')
 
     plt.tight_layout()
     plt.savefig(os.path.join('results', 'results_' + file_name + '.jpg'))
@@ -41,5 +60,5 @@ for files in os.scandir(image_root):
 
     counter += 1
 
-    if counter >= 6000: #6000
+    if counter >= 500: #6000
         break
